200b9868c3b0bf0b1afb7aed57b1c6eacf535ddd
ooro3841-gif <ooro3841@gmail.com>
2025-11-28 18:27:36 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,12 +1,10 @@
 require('dotenv').config()
 const express = require('express');
 const app = express();
-
 const PORT = process.env.PORT;
-
 app.get('/', (req, res) => {
   res.send('Server is running!');
 });
-
 app.listen(PORT, () => {
-  con
+  console.log(`Server started on port ${PORT}`);
+});
